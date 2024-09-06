b5fb96f58205c2315dee0e35fdca2b1fea5012d5
RodrigoSatler <rodrigo.satler@hotmail.com>
2024-09-06 16:29:37 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+      titulo: "Bia Souza",
+      descricao: "Bia Souza é uma judoca brasileira que conquistou o país ao trazer a primeira medalha de ouro olímpica para o Brasil nas Olimpíadas de Paris.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "paris medalha ouro"
+    },
+    {
+      titulo: "Mayra Aguiar",
+      descricao: "Mayra Aguiar é uma judoca brasileira de grande destaque internacional. Com diversas medalhas em campeonatos mundiais e pan-americanos, ela é uma das principais atletas da seleção brasileira de judô.",
+      link: "https://pt.wikipedia.org/wiki/Mayra_Aguiar",
+      tags: "internacional mundiais "
+    },
+    {
+      titulo: "Rafaela Silva",
+      descricao: "Rafaela Silva é uma judoca brasileira conhecida por sua agilidade e técnica. Ela conquistou a medalha de ouro nas Olimpíadas do Rio de Janeiro em 2016 e é considerada uma das maiores atletas do judô brasileiro.",
+      link: "https://pt.wikipedia.org/wiki.org/Rafaela_Silva",
+      tags:"rio 2016 judo agilidade "
+    }
+  ];
+
+ 
+  
